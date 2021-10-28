2bcc6e8c046a9cdc4f835875d51513d2e4ebab47
astridx <info@astrid-guenther.de>
2021-10-28 12:22:51 +0200

    wip

--- public/sw.js
+++ public/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5d43e1df045605d24160.js"
+    "url": "webpack-runtime-5b70cad0a5335659e3fc.js"
   },
   {
     "url": "framework-b17a64ad725e0b3c1338.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b8a2b0d5114ddcf07a9e2675aed1c8a5"
+    "revision": "6bd9796a33f490da8bcf1a3d22eac072"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
--- src/data/books.js
+++ src/data/books.js
@@ -1,6 +1,18 @@
 export default [
   {
     meta: 'Self-Publishing',
+    title: 'Joomla 4 – Developing Extensions : Step by step to a working Joomla extension',
+    path:
+      'https://www.amazon.com/-/de/dp/B09F27HFFG/ref=sr_1_1?dchild=1&qid=1635415914&qsid=142-8519792-6387715&refinements=p_27%3AAstrid+G%C3%BCnther&s=digital-text&sr=1-1&sres=B09F27HFFG%2CB09FKQDQFS%2CB089Y2T65Q%2CB07MXMDS13%2CB089Y519ZM%2CB08F3CZWW4%2CB0973J65ZQ%2CB0939VR5LJ%2CB096LC555J%2CB096LBV4VC%2CB08SQL4WJ1%2CB082PW6QWN%2CB01FHRPQ6I%2CB00UZBDVTQ%2CB098XM9Z16%2CB098QK6DXC&text=Astrid+G%C3%BCnther',
+  },
+  {
+    meta: 'Self-Publishing',
+    title: 'Joomla 4 - Erweiterungen programmieren',
+    path:
+      'https://www.amazon.de/Joomla-Erweiterungen-programmieren-lauff%C3%A4higen-Erweiterung/dp/3754153331/?_encoding=UTF8&pd_rd_w=9XJv6&pf_rd_p=bf172aca-3277-41f6-babb-6ce7fc34cf7f&pf_rd_r=WXAWGQHB9EH979YRR3JP&pd_rd_r=525300e3-b270-4900-bf7f-0b95c83d5c65&pd_rd_wg=kahdI&ref_=pd_gw_ci_mcx_mr_hp_atf_m',
+  },
+  {
+    meta: 'Self-Publishing',
     title: 'Leaflet',
     path:
       'https://www.epubli.de/shop/buch/Leaflet---Zweite-v%C3%B6llig-%C3%BCberarbeitete-Version-Astrid-G%C3%BCnther-9783748507451/83153',
